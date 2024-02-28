face94c64e17ff3f932eab3ce921cf72d37b3e9d
pavan <2023bit505@sggs.ac.in>
2024-02-28 22:34:12 +0530

    these is javatpoint reference example

---
+++ referencdevariable.java
@@ -0,0 +1,15 @@
+class Car {
+    String brand;
+    int year;
+
+    public static void main(String []args){
+        Car mycCar;
+        mycCar=new Car();
+        mycCar.brand="Toyota";
+        mycCar.year=1947;
+        System.out.println("Brand:"+mycCar.brand);
+        System.out.println("Year:"+mycCar.year);
+    }
+}
+
+
